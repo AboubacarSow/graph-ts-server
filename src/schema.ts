9c357cd32095cb3b72477df1eb9aae86b1dc7daf
ABOUBACAR SOW <175620651+AboubacarSow@users.noreply.github.com>
2026-02-13 20:55:29 +0300

    update sever and schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,18 +1,20 @@
-const schema = `
+import { gql } from 'graphql-tag';
+
+const schema = gql`
   type Car {
-    id: ID!,
-    carType: String!,
+    id: ID!
+    carType: String!
     color: String!
     year: Int!
   }
 
   type Query {
-    getCars : [Car!]!
+    getCars: [Car!]!
   }
 
   type Mutation {
-    addCar(carType : String! , color : String! , year : Int!) : Car!
+    addCar(carType: String!, color: String!, year: Int!): Car!
   }
 `;
 
-export default schema
+export default schema;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import { ApolloServer } from '@apollo/server'
 import express from 'express'
 import cors from 'cors'
+import http from 'http'
 import {expressMiddleware} from '@as-integrations/express4'
-import bodyParser from 'body-parser';
 import schema from './schema'
 import resolvers from './resolver'
 
@@ -10,6 +10,7 @@ import resolvers from './resolver'
 
 async function startAppolloServer(schema: any, resolvers: any) {
     const app = express()
+    const httpServer = http.createServer(app)
     const server = new ApolloServer({
         typeDefs:schema,
         resolvers,
@@ -18,15 +19,15 @@ async function startAppolloServer(schema: any, resolvers: any) {
     }) as any
 
     await server.start();
+    app.use(cors())
+    app.use(express.json())
     app.use(
         '/graphql',
-        cors(),
-        bodyParser.json(),
         expressMiddleware(server)
     );
 
-    app.listen(4000, () => {
-        console.log('🚀 Server running at http://localhost:4000/graphql');
-    });
+    await new Promise<void>((resolve)=> 
+        httpServer.listen({ port: 4000 }, resolve));
+    console.log(`🚀 Server ready at http://localhost:4000/graphql`);
 }
 startAppolloServer(schema,resolvers)
